54a7c0089c649e6d1713f4ab74b0c455e2a26279
Andi Miller <andi@andimiller.net>
2015-07-13 18:04:19 +0100

    Initial checkin

---
+++ build.sbt
@@ -0,0 +1,14 @@
+name := "pizza-sov-relay"
+
+version := "1.0"
+
+scalaVersion := "2.10.4"
+
+resolvers ++= Seq(
+  "Sonatype snapshots repository" at "https://oss.sonatype.org/content/repositories/snapshots/",
+  "Spray repository" at "http://repo.spray.io/",
+  "Typesafe repository" at "http://repo.typesafe.com/typesafe/releases/"
+)
+
+libraryDependencies += "io.backchat.hookup" % "hookup_2.10" % "0.4.0"
+libraryDependencies += "net.databinder.dispatch" %% "dispatch-core" % "0.11.2"
---
+++ src/main/scala/SovRelay.scala
@@ -0,0 +1,57 @@
+import java.util.concurrent.atomic.AtomicInteger
+
+import io.backchat.hookup._
+import scala.concurrent.duration._
+import dispatch._, Defaults._
+
+import scala.util.{Failure, Try}
+
+object SovRelay extends App {
+
+  case class Broadcast(x: String)
+
+  var lastUpdate: String = "{\"status\": \"starting up\"}"
+  var users = new AtomicInteger(0)
+
+  val server = HookupServer(8125) {
+    new HookupServerClient {
+      def receive = {
+        case Connected =>
+          send(lastUpdate)
+          users.incrementAndGet()
+        case TextMessage(text) =>
+          println(text)
+          send("{\"error\": \"this server cannot perform user-requested actions (except this one)\"}")
+        case Disconnected(why) =>
+          println("user disconnected")
+          println(why)
+          users.decrementAndGet()
+      }
+    }
+  }
+  server.start
+
+  val system = akka.actor.ActorSystem("system")
+
+  implicit class EitherPimp[L <: Throwable,T](e:Either[L,T]) {
+    def toTry:Try[T] = e.fold(Failure(_), util.Success(_))
+  }
+
+  def pullLatest(): Unit = {
+    val svc = url("https://public-crest-duality.testeveonline.com/sovereignty/structures/")
+    val res = Http(svc OK as.String)
+    res.either.map {
+      case Right(r) =>
+        lastUpdate = r
+        server.broadcast(new TextMessage(r))
+      case Left(t) =>
+        println(t.getMessage)
+        println(t)
+        server.broadcast(new TextMessage("{\"status\": \"ccp api is unavailable\"}"))
+    }
+  }
+
+  system.scheduler.schedule(1 seconds, 300 seconds)(pullLatest)
+  println("Scheduled puller")
+  server
+}
